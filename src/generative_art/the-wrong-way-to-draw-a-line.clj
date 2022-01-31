69171a275c311e2631f0c0100c0df241599ba546
kero_hirake <k16.h.white@gmail.com>
2022-02-01 02:14:05 +0900

    少し連続したノイズ

--- src/generative_art/the-wrong-way-to-draw-a-line.clj
+++ src/generative_art/the-wrong-way-to-draw-a-line.clj
@@ -2,7 +2,7 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
-(defn setup []
+(defn setup-random1 []
   (q/frame-rate 30)
   ;(q/color-mode :hsb)
   (q/background 255)
@@ -10,23 +10,42 @@
   (q/smooth)
 
   (q/stroke 20 50 70)
+
   (doseq [p (partition 2 1 (for [x (range 500)]
                              {:x x :y (+ 50 (rand-int 100))}))]
     (let [f (first p)
           s (second p)]
-      (q/line (:x f)  (:y f)  (:x s)  (:y s)))
-    ))
+      (q/line (:x f)  (:y f)  (:x s)  (:y s)))))
+
+(defn setup-random2 []
+  (q/frame-rate 30)
+  ;(q/color-mode :hsb)
+  (q/background 255)
+  (q/stroke-weight 1)
+  (q/smooth)
+
+  (q/stroke 20 50 70)
+
+  (doseq [p (partition 2 1 (reduce (fn [col n]
+                                     (let [l (last col)
+                                           y (+ (:y l) (- (rand-int 20) 9.5))]
+                                       (conj col {:x n :y y})))
+                                   [{:x 0 :y (/ (q/height) 2)}] (range (q/width))))]
+    (let [f (first p)
+          s (second p)]
+      (q/line (:x f)  (:y f)  (:x s)  (:y s)))))
+
+
 
 (defn update-state [state])
 
 (defn draw-state [state])
 
 
-
 (q/defsketch generative-art
   :title "the wrong way to draw a line"
   :size [500 500]
-  :setup setup
+  :setup setup-random2
   :update update-state
   :draw draw-state
   :features [:keep-on-top]
