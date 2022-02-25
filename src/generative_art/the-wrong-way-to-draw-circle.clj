0a1d70ca82e2949b2ca83364918a9cab4973b1da
kero_hirake <k16.h.white@gmail.com>
2022-02-25 22:16:24 +0900

    らせん

--- src/generative_art/the-wrong-way-to-draw-circle.clj
+++ src/generative_art/the-wrong-way-to-draw-circle.clj
@@ -2,7 +2,10 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
-
+(defn draw-line [points]
+  (doseq [p (partition 2 1 points)]
+    (let [[{x1 :x, y1 :y} {x2 :x, y2 :y}] p]
+      (q/line x1 y1 x2 y2))))
 
 (defn setup []
   (q/background 255)
@@ -13,14 +16,20 @@
   ;(q/ellipse 250 150 200 200)
 
   (q/stroke 20 50 70)
-  (let [radius 100
-        center-x (/ (q/width) 2)
-        center-y (/ (q/height) 2)]
-    (doseq [angle (range 0 361)]
-      (let [rad (q/radians angle)
-            x (+ center-x (* radius (q/cos rad)))
-            y (+ center-y (* radius (q/sin rad)))]
-        (q/point x y)))))
+
+  (let [center-x (/ (q/width) 2)
+        center-y (/ (q/height) 2)
+        radiuses (iterate #(+ % 0.5) 10)
+        angles (range 0 1441 5)
+        rads (map q/radians angles)
+        make-point (fn [radius rad]                     
+                     {:x (+ center-x (* (q/cos rad) radius))
+                      :y (+ center-y (* (q/sin rad) radius))})
+        points (map make-point radiuses rads)]
+    (draw-line points)))
+
+(comment 
+  )
 
 (defn update-state [state])
 
